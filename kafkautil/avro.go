28c4ea78bedeffc860563e541720cc86d775d29e
Richy Great <richygreat@gmail.com>
2023-01-28 22:52:24 +0100

    New value avro specific serializer #7

--- kafkautil/avro.go
+++ kafkautil/avro.go
@@ -9,19 +9,43 @@ import (
 )
 
 func NewValueAvroSpecificDeserializer() *avro.SpecificDeserializer {
-	schemaRegistryAPIUrl := os.Getenv("SCHEMA_REGISTRY_URL")
-	schemaRegistryAPIKey := os.Getenv("SCHEMA_REGISTRY_API_KEY")
-	schemaRegistryAPISecret := os.Getenv("SCHEMA_REGISTRY_API_SECRET")
-	client, err := schemaregistry.NewClient(schemaregistry.NewConfigWithAuthentication(
-		schemaRegistryAPIUrl, schemaRegistryAPIKey, schemaRegistryAPISecret))
-	if err != nil {
-		fmt.Printf("NewValueAvroSpecificDeserializer failed to create schema registry client with error=%s\n", err)
-		os.Exit(1)
-	}
+	client := newSchemaRegistryClient()
 	deser, err := avro.NewSpecificDeserializer(client, serde.ValueSerde, avro.NewDeserializerConfig())
 	if err != nil {
 		fmt.Printf("NewValueAvroSpecificDeserializer failed to create deserializer with error=%s\n", err)
 		os.Exit(1)
 	}
 	return deser
+}
+
+func NewValueAvroSpecificSerializer() *avro.SpecificSerializer {
+	client := newSchemaRegistryClient()
+	ser, err := avro.NewSpecificSerializer(client, serde.ValueSerde, newSerializerConfig())
+	if err != nil {
+		fmt.Printf("NewValueAvroSpecificSerializer failed to create serializer with error=%s\n", err)
+		os.Exit(1)
+	}
+	return ser
+}
+
+func newSchemaRegistryClient() schemaregistry.Client {
+	schemaRegistryAPIUrl := os.Getenv("SCHEMA_REGISTRY_URL")
+	schemaRegistryAPIKey := os.Getenv("SCHEMA_REGISTRY_API_KEY")
+	schemaRegistryAPISecret := os.Getenv("SCHEMA_REGISTRY_API_SECRET")
+	client, err := schemaregistry.NewClient(schemaregistry.NewConfigWithAuthentication(
+		schemaRegistryAPIUrl, schemaRegistryAPIKey, schemaRegistryAPISecret))
+	if err != nil {
+		fmt.Printf("NewValueAvroSpecificSerializer failed to create schema registry client with error=%s\n", err)
+		os.Exit(1)
+	}
+	return client
+}
+
+func newSerializerConfig() *avro.SerializerConfig {
+	c := &avro.SerializerConfig{}
+	c.AutoRegisterSchemas = false
+	c.UseSchemaID = -1
+	c.UseLatestVersion = true
+	c.NormalizeSchemas = false
+	return c
 }
