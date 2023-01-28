9df07318c96e70b0a4f3d0a2e78045b4135fd1ec
Richy Great <richygreat@gmail.com>
2023-01-28 09:47:39 +0100

    Rebalance util for confluent kafka partitions #1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/softwok/confluent-kafka-util-go
+
+go 1.19
+
+require github.com/confluentinc/confluent-kafka-go/v2 v2.0.2
---
+++ rebalance.go
@@ -0,0 +1,31 @@
+package kafkautil
+
+import (
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func RebalanceCallback(c *kafka.Consumer, event kafka.Event) error {
+	switch ev := event.(type) {
+	case kafka.AssignedPartitions:
+		fmt.Printf("Rebalanced new partition(s) assigned=%v\n", ev.Partitions)
+
+		// The application may update the start .Offset of each
+		// assigned partition and then call IncrementalAssign().
+		// Even though this example does not alter the offsets we
+		// provide the call to IncrementalAssign() as an example.
+		err := c.IncrementalAssign(ev.Partitions)
+		if err != nil {
+			panic(err)
+		}
+
+	case kafka.RevokedPartitions:
+		fmt.Printf("Rebalanced old partition(s) revoked=%v\n", ev.Partitions)
+		if c.AssignmentLost() {
+			// Our consumer has been kicked out of the group and the entire assignment is thus lost.
+			fmt.Println("Current assignment lost!")
+		}
+		// The client automatically calls IncrementalUnassign() unless the callback has already called that method.
+	}
+	return nil
+}
