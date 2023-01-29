f5dcbfd2925d6b2eda7f631d93a2eefb95e23f9a
Richy Great <richygreat@gmail.com>
2023-01-29 10:30:40 +0100

    Union schema specific deser util #8

--- kafkautil/avro.go
+++ kafkautil/avro.go
@@ -1,11 +1,18 @@
 package kafkautil
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/actgardner/gogen-avro/v10/compiler"
+	"github.com/actgardner/gogen-avro/v10/parser"
+	"github.com/actgardner/gogen-avro/v10/resolver"
+	"github.com/actgardner/gogen-avro/v10/schema"
+	"github.com/actgardner/gogen-avro/v10/vm"
 	"github.com/confluentinc/confluent-kafka-go/v2/schemaregistry"
 	"github.com/confluentinc/confluent-kafka-go/v2/schemaregistry/serde"
 	"github.com/confluentinc/confluent-kafka-go/v2/schemaregistry/serde/avro"
 	"os"
+	"strings"
 )
 
 func NewValueAvroSpecificDeserializer() *avro.SpecificDeserializer {
@@ -28,6 +35,43 @@ func NewValueAvroSpecificSerializer() *avro.SpecificSerializer {
 	return ser
 }
 
+func DeserializeInto(s *avro.SpecificDeserializer, topic string, payload []byte, msg interface{}) error {
+	if payload == nil {
+		return nil
+	}
+	var avroMsg avro.SpecificAvroMessage
+	switch t := msg.(type) {
+	case avro.SpecificAvroMessage:
+		avroMsg = t
+	default:
+		return fmt.Errorf("serialization target must be an avro message. Got '%v'", t)
+	}
+	info, err := s.GetSchema(topic, payload)
+	if err != nil {
+		return err
+	}
+	reader, err := toAvroType(s, schemaregistry.SchemaInfo{Schema: avroMsg.Schema()})
+	if err != nil {
+		return err
+	}
+	for _, reference := range info.References {
+		if strings.HasSuffix(reference.Name, reader.Name()) {
+			metadata, err := s.Client.GetLatestSchemaMetadata(reference.Subject)
+			if err != nil {
+				return err
+			}
+			writer, err := toAvroType(s, schemaregistry.SchemaInfo{Schema: metadata.Schema})
+			deser, err := compiler.Compile(writer, reader)
+			if err != nil {
+				return err
+			}
+			r := bytes.NewReader(payload[5:])
+			return vm.Eval(r, deser, avroMsg)
+		}
+	}
+	return nil
+}
+
 func newSchemaRegistryClient() schemaregistry.Client {
 	schemaRegistryAPIUrl := os.Getenv("SCHEMA_REGISTRY_URL")
 	schemaRegistryAPIKey := os.Getenv("SCHEMA_REGISTRY_API_KEY")
@@ -48,4 +92,38 @@ func newSerializerConfig() *avro.SerializerConfig {
 	c.UseLatestVersion = true
 	c.NormalizeSchemas = false
 	return c
+}
+
+func toAvroType(s *avro.SpecificDeserializer, schema schemaregistry.SchemaInfo) (schema.AvroType, error) {
+	ns := parser.NewNamespace(false)
+	return resolveAvroReferences(s.Client, schema, ns)
+}
+
+func resolveAvroReferences(c schemaregistry.Client, schema schemaregistry.SchemaInfo, ns *parser.Namespace) (schema.AvroType, error) {
+	for _, ref := range schema.References {
+		metadata, err := c.GetSchemaMetadata(ref.Subject, ref.Version)
+		if err != nil {
+			return nil, err
+		}
+		info := schemaregistry.SchemaInfo{
+			Schema:     metadata.Schema,
+			SchemaType: metadata.SchemaType,
+			References: metadata.References,
+		}
+		_, err = resolveAvroReferences(c, info, ns)
+		if err != nil {
+			return nil, err
+		}
+
+	}
+	sType, err := ns.TypeForSchema([]byte(schema.Schema))
+	if err != nil {
+		return nil, err
+	}
+	for _, def := range ns.Roots {
+		if err := resolver.ResolveDefinition(def, ns.Definitions); err != nil {
+			return nil, err
+		}
+	}
+	return sType, nil
 }
